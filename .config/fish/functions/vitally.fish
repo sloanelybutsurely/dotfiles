dc9b95dbb36aeb335b443cfd81888f21cc3dd9d2
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-03-15 14:09:30 -0400

    add init to vitally scripts shorthand

--- .config/fish/functions/vitally.fish
+++ .config/fish/functions/vitally.fish
@@ -1,8 +1,8 @@
 function vitally
   set cmd $argv[1]
-  set cmds start stop session-name
+  set cmds start stop session-name init
   if not contains $cmd $cmds
-    echo "usage: vitally <CMD>; <CMD>: start, stop, session-name"
+    echo "usage: vitally <CMD>; <CMD>: start, stop, session-name, init"
   else
     eval "vitally-$cmd"
   end
