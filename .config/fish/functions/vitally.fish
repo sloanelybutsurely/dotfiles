834465a89d41875d82ece3ea712b322e6e68593e
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-03-19 09:25:20 -0400

    extract prefixed function behavior

--- .config/fish/functions/vitally.fish
+++ .config/fish/functions/vitally.fish
@@ -1,18 +1,3 @@
 function vitally
-  set cmd $argv[1]
-
-  if not contains $cmd start stop session-name init do
-    echo "usage: vitally <CMD>; <CMD>: start, stop, session-name, init"
-    return 1
-  end
-
-  if test $cmd = "do"
-    set cmds (string replace -a ',' '' $argv[2..])
-  else
-    set cmds $cmd
-  end
-
-  for c in $cmds
-    eval "vitally-$c"
-  end
+  prefixed-function vitally $argv[1..]
 end
