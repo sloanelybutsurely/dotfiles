9872a31ac2ae4763f9c2cc527702c6ba42a14cac
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-04-06 08:33:53 -0400

    install vim-jjdescription

--- .config/nix-darwin/modules/darwin/nixvim.nix
+++ .config/nix-darwin/modules/darwin/nixvim.nix
@@ -27,6 +27,18 @@
       vim-textobj-user
 
       nerdtree
+
+      (
+        pkgs.vimUtils.buildVimPlugin {
+          name = "vim-jjdescription";
+          src = pkgs.fetchFromGitHub {
+            owner = "avm99963";
+            repo = "vim-jjdescription";
+            rev = "c9bf9f849ead3961ae38ab33f68306996e64c6e8";
+            hash = "sha256-qnZFuXbzpm2GN/+CfksFfW2O+qTosUZcUagqCTzmtWo=";
+          };
+        }
+      )
     ];
 
     plugins = {
