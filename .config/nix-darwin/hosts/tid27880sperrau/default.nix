d9d21a7462e53e446c3a2d0dee88dd91bcfc8d19
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-05-21 12:19:42 -0400

    install httpie, awscli

--- .config/nix-darwin/hosts/tid27880sperrau/default.nix
+++ .config/nix-darwin/hosts/tid27880sperrau/default.nix
@@ -2,7 +2,14 @@
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  environment.systemPackages = with pkgs; [
+    httpie
+  ];
+
   homebrew = {
+    brews = [
+      "awscli"
+    ];
     casks = [
       "slack"
     ];
