37d91c20c4d2f4e5d9747019b11e21fc1ca8ad0c
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-07-24 07:39:22 -0400

    reorder shell util hooks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,10 @@ if status is-interactive
   fish_vi_key_bindings
   mise activate fish | source
   mise hook-env -s fish | source
-  jj util completion fish | source
-
+  direnv hook fish | source
   zoxide init fish | source
+
+  jj util completion fish | source
   
   if not set -q TMUX
     set -g TMUX tmux new-session -d -s default
