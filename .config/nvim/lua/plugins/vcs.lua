f57e80a179c97d02488237278f94ac4de7d5befb
sloane <git@sloanelybutsurely.com>
2024-08-21 10:58:57 -0400

    install hunk, ignore .iex.exs, update packages

--- .config/nvim/lua/plugins/vcs.lua
+++ .config/nvim/lua/plugins/vcs.lua
@@ -1,4 +1,12 @@
 return {
   { 'tpope/vim-fugitive' },
   { 'tpope/vim-rhubarb' },
+  {
+    'julienvincent/hunk.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    cmd = { 'DiffEditor' },
+    config = function ()
+      require('hunk').setup()
+    end,
+  },
 }
