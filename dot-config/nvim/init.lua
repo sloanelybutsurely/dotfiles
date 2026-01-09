b13aee59efc9535b9461c71bf036541b6fd1f648
sloane <git@sloanelybutsurely.com>
2026-01-09 10:40:08 -0500

    nvim,zellij: add nvim+zellij navigation, use autolock

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -97,6 +97,24 @@ require("lazy").setup({
       })
     end
   },
+
+  {
+    "swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    keys = {
+      { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>",  { silent = true, desc = "navigate left or tab"  } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down"  } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up"    } },
+      { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+    },
+    opts = {},
+  },
 })
 
 vim.cmd.colorscheme "catppuccin"
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "*",
+  command = "silent !zellij action switch-mode normal"
+})
