5fd2dc275c9bcc1f35c3d5fa34b27fc8d134dbdb
sloane <git@sloanelybutsurely.com>
2026-04-04 14:21:44 -0400

    neovim: uninstall nvim-treesitter (abandoned)

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -143,21 +143,6 @@ require("lazy").setup({
       })
     end,
   },
-  
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  },
 })
 
 vim.cmd.colorscheme "catppuccin"
