709925e8d3e1e7c55c2e8fe21b5956dac976300e
sloane <sloane@sloanelybutsurely.com>
2026-05-20 09:30:47 -0400

    fish: move abbr configuration, remove LM_STUDIO configuration

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -5,12 +5,7 @@ mise activate fish | source
 if status is-interactive
 	fish_vi_key_bindings
 	zoxide init fish | source
-
-  abbr j jj
-  abbr hex hexyl
 end
 
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/sloane/.lmstudio/bin
-# End of LM Studio CLI section
+abbr j jj
 
