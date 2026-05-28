46fa0272001a2d13e08ac682c28cfe92d628e5fb
sloane <sloane@sloanelybutsurely.com>
2026-05-28 12:23:02 -0400

    mise,pnpm: update pnpm, add pnpm/bin to path (automatic)

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -11,3 +11,10 @@ abbr j jj
 
 set -gx EDITOR nvim
 set -gx MANPAGER 'nvim +Man!'
+
+# pnpm
+set -gx PNPM_HOME "/Users/sloane/Library/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
