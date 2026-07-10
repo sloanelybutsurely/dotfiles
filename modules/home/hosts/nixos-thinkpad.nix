965f4a6faa9d3b581bb0c5a85503e4c96603c034
sloane <sloane@sloanelybutsurely.com>
2026-07-10 15:49:00 -0400

    nixos(thinkpad): remove commented out waybar settings

--- modules/home/hosts/nixos-thinkpad.nix
+++ modules/home/hosts/nixos-thinkpad.nix
@@ -1,8 +1,2 @@
 { config, pkgs, ... }: {
-  # programs.waybar.settings.mainBar = {
-  #   modules-right = [ "mpd" "cpu" "memory" "battery#bat0" "battery#bat1" "clock" "tray" ];
-  #
-  #   "battery#bat0"."bat" = "BAT0";
-  #   "battery#bat1"."bat" = "BAT1";
-  # };
 }
